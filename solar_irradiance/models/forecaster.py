5e1c0c1c65327f05a9db9c4f6d06c97b432c30d7
Mateusz <piechocki.mateusz97@gmail.com>
2023-08-11 12:53:32 +0200

    Remove code duplication

--- solar_irradiance/models/forecaster.py
+++ solar_irradiance/models/forecaster.py
@@ -34,27 +34,15 @@ class Forecaster(pl.LightningModule):
 
         if model_name == 'swin3d_b':
             self.network = swin3d_b(weights=Swin3D_B_Weights.KINETICS400_IMAGENET22K_V1, progress=True)
+            self.num_features = self.network.num_features
             self.network.head = torch.nn.Identity()
-            self.network_head = torch.nn.Sequential(
-                torch.nn.Linear(self.network.num_features + 4, 256),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Linear(256, 1),
-            )
         elif model_name == 'r3d_18':
             self.network = r3d_18(weights=R3D_18_Weights.KINETICS400_V1, progress=True, in_channels=self._input_channels)
-            self.network_head = torch.nn.Sequential(
-                torch.nn.Linear(self.network.fc.in_features + 4, 256),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Linear(256, 1),
-            )
+            self.num_features = self.network.fc.in_features
             self.network.fc = torch.nn.Identity()
         elif model_name == 'mc3_18':
             self.network = mc3_18(weights=MC3_18_Weights.KINETICS400_V1, progress=True, in_channels=self._input_channels)
-            self.network_head = torch.nn.Sequential(
-                torch.nn.Linear(self.network.fc.in_features + 4, 256),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Linear(256, 1),
-            )
+            self.num_features = self.network.fc.in_features
             self.network.fc = torch.nn.Identity()
         elif model_name.startswith('timm-'):
             self.network = timm.create_model(
@@ -63,12 +51,15 @@ class Forecaster(pl.LightningModule):
                 num_classes=1,
                 in_chans=self._input_channels,
             )
-            self.network_head = torch.nn.Sequential(
-                torch.nn.Linear(self.network.fc.in_features + 4, 256),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Linear(256, 1),
-            )
+            self.num_features = self.network.fc.in_features
             self.network.fc = torch.nn.Identity()
+
+        self.num_features += 4  # Add 4 historical irradiances
+        self.network_head = torch.nn.Sequential(
+            torch.nn.Linear(self.num_features, 256),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(256, 1),
+        )
 
         if loss_function == 'MSE':
             self.loss = torch.nn.MSELoss()
