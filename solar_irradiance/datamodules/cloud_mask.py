24858652f79fe4e9208ba85fe5281cde0d90c918
Mateusz <piechocki.mateusz97@gmail.com>
2023-08-16 12:15:08 +0200

    Add tests for cloud mask gen; update BR ratio calculation

--- solar_irradiance/datamodules/cloud_mask.py
+++ solar_irradiance/datamodules/cloud_mask.py
@@ -31,16 +31,18 @@ class CloudMask:
         return np.sqrt((self.shape[1]//2 - x_idx)**2 + (self.shape[0]//2 - y_idx)**2)
     
     def blue_red_ratio(self, img) -> np.ndarray:
-        mask = np.where(self.dist_mask > self.shape[0]//2, 0, img[..., 2] / np.add(img[..., 0], [1]))
+        img = img.astype(np.float32)
+        mask = np.where(self.dist_mask > self.shape[0]//2, 0, np.divide(img[..., 2], img[..., 0], out=np.zeros_like(img[..., 2]), where=img[..., 0]!=0))
         return (mask / 255).astype(np.float32)
 
     def blue_red_difference(self, img) -> np.ndarray:
         mask = np.where(self.dist_mask > self.shape[0]//2, 0, img[..., 2] - img[..., 0])
-        return ((mask + 255) / 510).astype(np.float32)
+        return (mask / 255).astype(np.float32)
 
     def normalized_blue_red_ratio(self, img) -> np.ndarray:
         # https://journals.ametsoc.org/view/journals/atot/28/10/jtech-d-11-00009_1.xml
-        br_ratio = img[..., 2] / np.add(img[..., 0], [1])
+        img = img.astype(np.float32)
+        br_ratio = np.divide(img[..., 2], img[..., 0], out=np.zeros_like(img[..., 2]), where=img[..., 0]!=0)
         mask = np.where(self.dist_mask > self.shape[0]//2, 1, (br_ratio - 1) / (br_ratio + 1))
         mask = np.where((-0.03 < mask) & (mask < 0.03), 255, 0).astype(np.uint8)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones((5,5), np.uint8), iterations=3)
