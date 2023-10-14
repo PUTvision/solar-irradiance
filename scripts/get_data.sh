58e19b7554bb3ef63ef25fd895078330afadd1da
Mateusz <piechocki.mateusz97@gmail.com>
2023-10-14 15:01:27 +0200

    Fix issues in dvc stages

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -10,21 +10,21 @@ echo "Downloading Folsom_irradiance.csv ..." && \
 
 # download sky images
 echo "Downloading and extracting Folsom_sky_images_2014.tar.bz2 ..." && \
-    # wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2014.tar.bz2 && \
+    wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2014.tar.bz2 && \
     echo "Folsom_sky_images_2014.tar.bz2 downloaded" && \
     tar xf Folsom_sky_images_2014.tar.bz2 --directory ./images/ && \
     echo "Folsom_sky_images_2014.tar.bz2 extracted from archive" && \
     rm Folsom_sky_images_2014.tar.bz2
 
 echo "Downloading and extracting Folsom_sky_images_2015.tar.bz2 ..." && \
-    # wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2015.tar.bz2 && \
+    wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2015.tar.bz2 && \
     echo "Folsom_sky_images_2015.tar.bz2 downloaded" && \
     tar xf Folsom_sky_images_2015.tar.bz2 --directory ./images/ && \
     echo "Folsom_sky_images_2015.tar.bz2 extracted from archive" && \
     rm Folsom_sky_images_2015.tar.bz2
 
 echo "Downloading and extracting Folsom_sky_images_2016.tar.bz2 ..." && \
-    # wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2016.tar.bz2 && \
+    wget https://zenodo.org/records/2826939/files/Folsom_sky_images_2016.tar.bz2 && \
     echo "Folsom_sky_images_2016.tar.bz2 downloaded" && \
     tar xf Folsom_sky_images_2016.tar.bz2 --directory ./images/ && \
     echo "Folsom_sky_images_2016.tar.bz2 extracted from archive" && \
