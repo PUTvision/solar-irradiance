aed0e88942f25a1e972405080c2a289a504b8379
Mateusz <piechocki.mateusz97@gmail.com>
2024-02-17 12:58:57 +0000

    Control target forecasting horizon and history size directly from config

--- scripts/export_periods.py
+++ scripts/export_periods.py
@@ -9,16 +9,13 @@ from tqdm import tqdm
 @click.command()
 @click.option('--cleaned-dataframe-path', type=click.Path(exists=True, path_type=Path), required=True)
 @click.option('--history-size', type=int, default=4)
+@click.option('--time-shift', type=int, default=5)
 @click.option('--time-window', type=int, default=15)
 @click.option('--output-path', type=click.Path(path_type=Path), required=True)
-def export_periods(cleaned_dataframe_path: Path, history_size: int, time_window: int, output_path: Path):
-    history_size -= 1   # history samples without sample from t time
-    period = time_window // history_size
-
+def export_periods(cleaned_dataframe_path: Path, history_size: int, time_shift: int, time_window: int, output_path: Path):
     periods = []
 
-    df = pd.read_csv(cleaned_dataframe_path, parse_dates=['datetime'],
-                     index_col='datetime')
+    df = pd.read_csv(cleaned_dataframe_path, parse_dates=['datetime'], index_col='datetime')
 
     print(f'Min irradiance: {df["ghi"].min()}')
     print(f'Max irradiance: {df["ghi"].max()}')
@@ -27,7 +24,7 @@ def export_periods(cleaned_dataframe_path: Path, history_size: int, time_window:
 
     for t, _ in tqdm(df.iterrows(), total=len(df)):
         t = pd.Timestamp(t)
-        t_history = [t - pd.Timedelta(minutes=period * i) for i in range(history_size, 0, -1)]
+        t_history = [t - pd.Timedelta(minutes=time_shift * i) for i in range(history_size - 1, 0, -1)]
         t_target = t + pd.Timedelta(minutes=time_window)
 
         if all(map(lambda x: x in df.index, [*t_history, t_target])):
